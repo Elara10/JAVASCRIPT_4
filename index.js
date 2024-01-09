8eae681d6e2e5a2c4c7163b9213130197064b920
IshaKumari10 <isha02740@gmail.com>
2024-01-09 23:20:25 +0530

    js datatypes

--- index.js
+++ index.js
@@ -22,6 +22,55 @@ multi
 line 
 comment
 */
-      var number1=34;
-      var number2=56;
-      console.log(number1+number2);
+    //   var number1=34;
+    //   var number2=56;
+    //   console.log(number1+number2);
+
+
+//4.Data types in js
+//Number
+var num1=455;
+var num2=56.78;
+
+//Strings
+var str1="This is a string";
+var str2='This is also a string';
+
+
+//Objects
+
+var marks={
+    A:56,
+    f:45
+}
+console.log(marks);
+
+//Booleans
+var a=true;
+var b=false;
+console.log(a,b);
+
+
+
+//undefined
+
+var und;
+console.log(undefined)
+
+
+//null
+var n=null;
+console.log(n);
+
+
+/*
+At a very high level,there are two types of data types in javascript
+1.Primitive data types:number,null,string,symbol,boolean,bigInt,undefined
+2.Reference or Non-Primitive data types:Array and  Objects
+*/
+
+
+
+
+
+var arr=[5,2,6,2]
