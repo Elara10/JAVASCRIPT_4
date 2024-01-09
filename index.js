e83abdc3d419d48d8d8ea9565eb7ab8e38333daa
IshaKumari10 <isha02740@gmail.com>
2024-01-09 23:00:10 +0530

    js variables

---
+++ index.js
@@ -0,0 +1,27 @@
+//1.WAYS TO PRINT IN JAVASCRIPT
+// console.log("Hello World");
+// alert("me");
+
+// document.write("this is document write")
+
+//2.JAVSCRIPT CONSOLE API
+
+// console.log("Hello World", 4 + 2, "Another log");
+// console.warn("this is a warning");
+// console.error("This is an error")
+
+
+
+//3.Javascript Variables
+//What are variables?-Containers to store data values.
+
+
+/*
+
+multi 
+line 
+comment
+*/
+      var number1=34;
+      var number2=56;
+      console.log(number1+number2);
